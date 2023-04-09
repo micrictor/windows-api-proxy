6f9c6efa8c38fc1050716d8f87f47d4f94445b85
Michael Torres <mic.ric.tor@gmail.com>
2023-04-08 18:19:30 -0700

    Update thunk based on work in FLS branch

--- thunk.h
+++ thunk.h
@@ -10,57 +10,72 @@
 // The first argument is the number of arguments for the target API.
 __attribute__((always_inline)) inline void JmpThunk(
     unsigned int apiArgsCount, UINT_PTR args[]) {
-    unsigned long long i; // Needs to be 64-bit value for the asm inline
-    UINT_PTR value;
-    UINT_PTR apiAddress = args[apiArgsCount];
-
-    // Preserve RBX (used for iterator) and RSI on the stack
-    asm inline("push %%rbx;"
-               "push %%rsi;"
+    // Preserve RBX, R12, R13, R14, R15, RSI, and RDI.
+    // These are the callee-saved registers, and we don't want to clobber them.
+    asm inline(
+        "push %%rbx;"
+        "push %%r12;"
+        "push %%r13;"
+        "push %%r14;"
+        "push %%r15;"
+        "push %%rsi;"
+        "push %%rdi;"
         : /* no output */
         : /* no input */
-        : "rbx", "rsi");
+        : "rbx", "r12", "r13", "r14", "r15", "rsi", "rdi");
+    unsigned long long i;
+    unsigned long long value;
+    // jmp rax is the standard way of jumping to a register address.
+    // RAX is also guaranteed to be caller-saved, so no risk of breakage.
+    register unsigned long long apiAddress asm("rax") = args[apiArgsCount];
 
+    // Store each parameter as specified by the Windows x64 Calling Convention.
+    // https://docs.microsoft.com/en-us/cpp/build/x64-calling-convention?view=vs-2019
+    // RCX, RDX, R8, R9, and the rest on the stack.
     for (i = 0; i < apiArgsCount; i++) {
-        // Store first argument in ECX, second in EDX, third in R8, fourth in R9,
-        // and the rest on the stack.
         value = args[i];
-        if (i == 0)
-            asm inline("movq %0, %%rcx;"
+        // Store the register values on the stack, we'll go get them later.
+        if (i <= 3)
+            asm inline(
+                "movq %%rsp, %%rdi;"
+                "subq %[i], %%rdi;"
+                "movq %[value],  (%%rdi);"
                 : /* no output */
-                : "r"(value)
-                : "rcx");
-        else if (i == 1)
-            asm inline("movq %0, %%rdx;"
-                : /* no output */
-                : "r"(value)
-                : "rdx");
-        else if (i == 2)
-            asm inline("movq %0, %%r8;"
-                : /* no output */
-                : "r"(value)
-                : "r8");
-        else if (i == 3)
-            asm inline("movq %0, %%r9;"
-                : /* no output */
-                : "r"(value)
-                : "r9");
+                : [value] "g"(value), [i] "g"((i+1) * 8)
+                : "rdi");
         // Push the rest of the arguments on the stack
-        // The offset is 16 + i * 0x8, which accounts for the two preserved
+        // The offset is (7*8) + i * 0x8, which accounts for the 7 preserved
         // 8-byte registers and the shadow store for the first 4 arguments.
         else {
-            asm inline("movq %[value], 0x10(%%rsp, %[i], 0x8);"
+            asm inline("movq %[value], 0x38(%%rsp, %[i], 0x8);"
                 : /* no output */
                 : [value] "g"(value), [i] "r"(i+1));
         }
     }
-    // Restore RBX and RSI, and jump to the target address.
+    // Grab our register values from the stack.
+    // Note that this may end up filling the argument registers with garbage,
+    // but that's OK because they'll just get ignored/cleaned up.
     asm inline(
+        "movq -0x08(%%rsp), %%rcx;"
+        "movq -0x10(%%rsp), %%rdx;"
+        "movq -0x18(%%rsp), %%r8;"
+        "movq -0x20(%%rsp), %%r9;"
+        : /* no output */
+        : /* no input */
+        : "rcx", "rdx", "r8", "r9");
+    // Restore callee-cleanup registers, and jump to the target address.
+    asm inline(
+        "pop %%rdi;"
         "pop %%rsi;"
+        "pop %%r15;"
+        "pop %%r14;"
+        "pop %%r13;"
+        "pop %%r12;"
         "pop %%rbx;"
         "jmp %0;"
         : /* no output */
-        : "r"(apiAddress));
+        : "r"(apiAddress)
+        : "rbx", "r12", "r13", "r14", "r15", "rsi", "rdi");
 }
 
 
